193270e585d5479bb29b87e288eb15572a83dd9c
Ant0C <cafeo97@gmail.com>
2023-03-22 16:12:28 +0100

    Lettura da file Json

--- server.php
+++ server.php
@@ -1,26 +1,33 @@
 <?php 
-
-$list = [
-    [
-    'todo' => 'HTML',
-    ],
-    [
-    'todo' => 'CSS',
-    ],
-    'todo' => 'Responsive Design',
-    [
-    'todo' => 'Javascript',
-    ],
-    [
-    'todo' => 'PHP',
-    ],
-    [
-    'todo' => 'Laravel',
-    ],
-];
-
 header('Content-Type: application/json');
 
-echo json_encode($list);
+// Con array presente in php
+// $list = [
+//     [
+//     'todo' => 'HTML',
+//     ],
+//     [
+//     'todo' => 'CSS',
+//     ],
+//     'todo' => 'Responsive Design',
+//     [
+//     'todo' => 'Javascript',
+//     ],
+//     [
+//     'todo' => 'PHP',
+//     ],
+//     [
+//     'todo' => 'Laravel',
+//     ],
+// ];
 
+//echo json_encode($list);
+
+// Con lettura da file Json
+// Leggiamo il file Json ed lo trsformiamo in stringa
+$todo_string = file_get_contents('./todolist.json');
+// Leggiamo la stringa preso dal file Json
+$todo_list = json_decode($todo_string, true);
+
+//var_dump($todo_list);
 ?>
